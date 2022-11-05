a0f75435d53aca6914de9a25741be76bcf20f3d5
MatthewOglesby <matt.oglesby99@gmail.com>
2022-11-05 09:28:42 -0500

    Pushing to someone else to fix thx

--- db/products.js
+++ db/products.js
@@ -6,34 +6,49 @@ async function getAllProducts(){
       SELECT * FROM products;
     `);
    
-
     return rows;
+
   } catch (error) {
-    throw (error);
+    throw ('error getting all products', error);
   }
 }
 
-async function createProduct({title, description, author, pageCount, genre, price, image}) {
-
+async function getProductById(id){
+  console.log(id)
   try {
-    const { rows: [product]} = await client.query(`
-      INSERT INTO products (title, description, author, pageCount, genre, price, image)
-      VALUES ($1, $2, $3, $4, $5, $6, $7)
-      RETURNING *;
-    `, [title, description, author, pageCount, genre, price, image])
-    console.log(product)
+    const {rows: [product]} = await client.query(`
+      SELECT * FROM products
+      WHERE id = $1;
+    `, [id]);
+  
     return product;
 
+  } catch (error) {
+    throw ('error in getting product by id', error);
+  }
+}
+
+async function createProduct({title, description, author, pageCount, genre, price, image, quantity}) {
+  try {
+    const { rows: [product]} = await client.query(`
+      INSERT INTO products (title, description, author, pageCount, genre, price, image, quantity)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      RETURNING *;
+    `, [title, description, author, pageCount, genre, price, image, quantity]);
+
+    // console.log(product);
+
+    return product;
   }
   catch(ex) {
     console.log('error in createProduct adapter function', ex)
   }
 }
 
-
 module.exports = {
   createProduct,
-  getAllProducts
+  getAllProducts,
+  getProductById
 }
 
 // title, description, author, page-count, genre
