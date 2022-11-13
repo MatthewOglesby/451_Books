e12473c15e42f42e493d2a072dd354ecc934e3ef
ethan <ethanaleroi@gmail.com>
2022-11-13 12:03:16 -0600

    working on cart.js in api

--- api/cart.js
+++ api/cart.js
@@ -1,10 +1,33 @@
 const express = require('express');
-const { getAllCarts } = require('../db/cart')
+const router = express.Router();
+const { getAllCarts, addProductToCart, updateCart, deleteCartItem } = require('../db/cart')
 
 router.get('/', async (req, res) => {
-    const users = await getAllCarts();
+    try {
+    const cart = await getAllCarts();
 
     res.send({
         cart
     });
+    }catch({order_quantity, productId}) {
+        next({order_quantity, productId});
+    }
 });
+
+router.post('/', async (req, res, next) => {
+    const { order_quantity, productId} = req.body;
+    try{
+    console.log('TESTING ABOVE CART');    
+    const cart = await addProductToCart({ order_quantity,  productId });
+
+
+    console.log("TESTING CART", cart);
+    res.send(cart);
+
+    }catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+module.exports = router;
