4f7628faf57978f62e48c284ca30607ef8b302dd
MatthewOglesby <matt.oglesby99@gmail.com>
2022-11-19 12:55:35 -0600

    Formatted navbar for readability

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,50 +9,18 @@ const Navbar = ({ logout, token, user, cartItems }) => {
         <h3 className="navbarName">451 Books</h3>
         {token ? (
           <>
-            {/* {
-                            isAdmin ? (
-                                <div></div>
-                            ) : (
-                                <div></div>
-                            )
-                        } */}
-            <Link to="/" className="navbarLink">
-              Home
-            </Link>
-            <Link to="/products" className="navbarLink">
-              Books
-            </Link>
-            <Link
-              to="/register"
-              onClick={() => {
-                logout();
-              }}
-              className="navbarLink"
-            >
-              Logout
-            </Link>
-
-            <Link to={`/cart/${id}`} className="navbarLink">
-              <ShoppingCartCheckoutIcon />
-            </Link>
+            <Link to="/" className="navbarLink">Home</Link>
+            <Link to="/products" className="navbarLink">Books</Link>
+            <Link to="/register" onClick={() => {logout();}} className="navbarLink">Logout</Link>
+            <Link to={`/cart/${id}`} className="navbarLink"><ShoppingCartCheckoutIcon /></Link>
           </>
         ) : (
           <>
-            <Link to="/" className="navbarLink">
-              Home
-            </Link>
-            <Link to="/products" className="navbarLink">
-              Books
-            </Link>
-            <Link to="/register" className="navbarLink">
-              Register
-            </Link>
-            <Link to="/login" className="navbarLink">
-              Login
-            </Link>
-            <Link to={`/cart/${id}`} className="navbarLink">
-              <ShoppingCartCheckoutIcon />
-            </Link>
+            <Link to="/" className="navbarLink">Home</Link>
+            <Link to="/products" className="navbarLink">Books</Link>
+            <Link to="/register" className="navbarLink">Register</Link>
+            <Link to="/login" className="navbarLink">Login</Link>
+            <Link to={`/cart/${id}`} className="navbarLink"><ShoppingCartCheckoutIcon /></Link>
           </>
         )}
       </nav>
