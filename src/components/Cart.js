8842b73061e027ecc94756bbd589f1a9f3cc56ed
Ross <rosskeene196@gmail.com>
2022-11-28 18:28:18 -0600

    delete working without

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,6 +6,7 @@ import Badge from "@mui/material/Badge";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import CartItem from "./CartItem";
 import cartIMG from "../Cart.jpeg";
 const Cart = ({ cartItems, token, fetchAllUserCartItems }) => {
   let total = 0;
@@ -20,94 +21,10 @@ const Cart = ({ cartItems, token, fetchAllUserCartItems }) => {
         <div className="cart-main-div">
           <div>{cartItems.length === 0 && <div>Cart Is Empty</div>}</div>
 
-          {cartItems.map((cartItem) => {
-            const { cartId, order_quantity, productId } = cartItem;
-            const [display, setDisplay] = useState("none");
-            const [count, setCount] = useState(order_quantity);
-
-            async function editCartItem(newCount) {
-              const updatedCartItems = {
-                order_quantity: newCount,
-              };
-              const result = await updateCart(token, updatedCartItems, cartId);
-              console.log(result);
-
-              fetchAllUserCartItems();
-            }
-
-            total = Math.round((total + cartItem.price * count) * 100) / 100;
-            return (
-              <div key={cartId} className="main-cart-div">
-                <div className="individualCartContainer">
-                  <Paper style={{ borderRadius: "1rem" }}>
-                    <img src={cartItem.image} className="cartProductImage" />
-                    <p>
-                      Qty: <Badge color="info" badgeContent={count}></Badge>{" "}
-                    </p>
-                    <p>Title: {cartItem.title}</p>
-                    <p>
-                      <strong>Price</strong> ${cartItem.price}
-                    </p>
-
-                    <ButtonGroup fullWidth={true}>
-                      <Button
-                        aria-label="reduce"
-                        onClick={(event) => {
-                          event.preventDefault();
-                          const newCount = Math.max(count - 1, 0);
-                          setCount(newCount);
-                          editCartItem(newCount);
-                        }}
-                      >
-                        <RemoveIcon />
-                      </Button>
-                      <Button
-                        aria-label="increase"
-                        onClick={(event) => {
-                          event.preventDefault();
-                          const newCount = Math.max(count + 1);
-                          editCartItem(newCount);
-                          setCount(newCount);
-                        }}
-                      >
-                        <AddIcon />
-                      </Button>
-                      <Button
-                        type="submit"
-                        color="error"
-                        variant="outlined"
-                        onClick={async (event) => {
-                          event.preventDefault();
-                          await deleteCartItem(token, cartId);
-                          fetchAllUserCartItems();
-                        }}
-                      >
-                        <DeleteOutlineIcon />
-                      </Button>
-                    </ButtonGroup>
-
-                    <Button 
-                    fullWidth={true}
-                      variant="outlined"
-                      style={{}}
-                      onClick={(event) => {
-                        event.preventDefault();
-                        if (display === "none") {
-                          setDisplay("block");
-                        } else {
-                          setDisplay("none");
-                        }
-                      }}
-                    >
-                      Click Description
-                    </Button>
-                    <div className="activity-box" style={{ display: display }}>
-                      <p>{cartItem.description}</p>
-                    </div>
-                  </Paper>
-                </div>
-              </div>
-            );
+          {cartItems?.map((cartItem, idx) => {
+            console.log("In MAP")
+            total = Math.round((total + cartItem.price * cartItem.order_quantity) * 100) / 100;
+          return <CartItem key={idx} cartItem={cartItem} token={token} fetchAllUserCartItems={fetchAllUserCartItems}/>
           })}
         </div>
         <div className="cart-total">
---
+++ src/components/CartItem.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button, Paper, ButtonGroup, Box } from "@mui/material";
+import { deleteCartItem, updateCart } from "../api";
+import Badge from "@mui/material/Badge";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import cartIMG from "../Cart.jpeg";
+const CartItem = ({ cartItem, token, fetchAllUserCartItems }) => {
+  
+
+  console.log("In MAP")
+  const { cartId, order_quantity, productId } = cartItem;
+  const [display, setDisplay] = useState("none");
+  const [count, setCount] = useState(order_quantity);
+
+  async function editCartItem(newCount) {
+    const updatedCartItems = {
+      order_quantity: newCount,
+    };
+    const result = await updateCart(token, updatedCartItems, cartId);
+    console.log(result);
+
+    fetchAllUserCartItems();
+  }
+
+
+      console.log("In CartItems")
+            return (
+              <div key={cartId} className="main-cart-div">
+                <div className="individualCartContainer">
+                  <Paper style={{ borderRadius: "1rem" }}>
+                    <img src={cartItem.image} className="cartProductImage" />
+                    <p>
+                      Qty: <Badge color="info" badgeContent={count}></Badge>{" "}
+                    </p>
+                    <p>Title: {cartItem.title}</p>
+                    <p>
+                      <strong>Price</strong> ${cartItem.price}
+                    </p>
+
+                    <ButtonGroup fullWidth={true}>
+                      <Button
+                        aria-label="reduce"
+                        onClick={(event) => {
+                          event.preventDefault();
+                          const newCount = Math.max(count - 1, 0);
+                          setCount(newCount);
+                          editCartItem(newCount);
+                        }}
+                      >
+                        <RemoveIcon />
+                      </Button>
+                      <Button
+                        aria-label="increase"
+                        onClick={(event) => {
+                          event.preventDefault();
+                          const newCount = Math.max(count + 1);
+                          editCartItem(newCount);
+                          setCount(newCount);
+                        }}
+                      >
+                        <AddIcon />
+                      </Button>
+                      <Button
+                        type="submit"
+                        color="error"
+                        variant="outlined"
+                        onClick={async (event) => {
+                          event.preventDefault();
+                          await deleteCartItem(token, cartId);
+                          fetchAllUserCartItems();
+                        }}
+                      >
+                        <DeleteOutlineIcon />
+                      </Button>
+                    </ButtonGroup>
+
+                    <Button 
+                    fullWidth={true}
+                      variant="outlined"
+                      style={{}}
+                      onClick={(event) => {
+                        event.preventDefault();
+                        if (display === "none") {
+                          setDisplay("block");
+                        } else {
+                          setDisplay("none");
+                        }
+                      }}
+                    >
+                      Click Description
+                    </Button>
+                    <div className="activity-box" style={{ display: display }}>
+                      <p>{cartItem.description}</p>
+                    </div>
+                  </Paper>
+                </div>
+              </div>
+            );
+   
+      
+    
+  
+};
+
+export default CartItem;
