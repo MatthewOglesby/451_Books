b037d0ac67ffa929e30897533163729f6b654d01
ceoconn <coconnell1661@gmail.com>
2022-12-07 21:13:46 -0700

    had to change carousel if statements to title since productID changes

--- src/components/SingleProductView.js
+++ src/components/SingleProductView.js
@@ -18,43 +18,43 @@ const SingleProductView = ({ products, user, fetchAllUserCartItems, navigate, to
         // console.log(isAdmin)
 
         // harry potter and the sorcerers stone
-        if (productID == 1) {
+        if (title == "Harry Potter and the Sorcerer's Stone") {
             image2 = 'https://books.google.com/books/content?id=wrOQLV6xB-wC&pg=PT1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U0dWP114sBa2ciuo7WqbQHSN0FZAA&w=1280';
             image3 = 'https://books.google.com/books/content?id=wrOQLV6xB-wC&pg=PT19&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1REJBeU68CnEEBEL67GeZDwVumNg&w=1280';
         }
 
         // life 3.0
-        if (productID == 7) {
+        if (title == 'Life 3.0') {
             image2 = 'https://books.google.com/books/publisher/content?id=2hIcDgAAQBAJ&pg=PA1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U0nGtgXHr2NOPSN-5pr-fWUxpEqwg&w=1280';
             image3 = 'https://books.google.com/books/publisher/content?id=2hIcDgAAQBAJ&pg=PA31&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U2bGvfjxPMgoj9Wx_2cwd4VAyYlLQ&w=1280';
         }
 
         // spongebob goes to the doctor
-        if (productID == 11) {
+        if (title == 'SpongeBob Goes to the Doctor') {
             image2 = 'https://books.google.com/books/publisher/content?id=9b1WEAAAQBAJ&pg=PT1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1fDcjmj3YI0fieHqCvc0wGtr7jkw&w=1280';
             image3 = 'https://books.google.com/books/publisher/content?id=9b1WEAAAQBAJ&pg=PT5&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U0aPLF-pa1wtZUjdT24v9lGaWZP7Q&w=1280';
         }
 
         // bone #1
-        if (productID == 16) {
+        if (title == "Bone #1: Out from Boneville") {
             image2 = 'https://books.google.com/books/content?id=_N9XRzAruOQC&pg=PP4&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U2yQCxJA3OjJw9XnLthvkRlqmONSA&w=1280';
             image3 = 'https://books.google.com/books/content?id=_N9XRzAruOQC&pg=PP5&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U2zMzmlrG522IzdpGmjx7TLM2Zp1w&w=1280';
         }
 
         // the last ronin
-        if (productID == 18) {
+        if (title == "Teenage Mutant Ninja Turtles: The Last Ronin") {
             image2 = 'https://books.google.com/books/publisher/content?id=lpgeEAAAQBAJ&pg=PT12&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U2RhAizGXRh04Rb8C_YFcU_ryMg8g&w=1280';
             image3 = 'https://books.google.com/books/publisher/content?id=lpgeEAAAQBAJ&pg=PT17&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U23PCKs9M966nws2bTLQBzC1l0NLA&w=1280';
         }
 
         // web design for dummies
-        if (productID == 28) {
+        if (title == "Building a Web Site For Dummies") {
             image2 = 'https://books.google.com/books/publisher/content?id=lUOWAAAAQBAJ&pg=PA14&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U0xq2qsFceLAOG6IHdJIBJ9P9Db_g&w=1280';
             image3 = 'https://books.google.com/books/publisher/content?id=lUOWAAAAQBAJ&pg=PA31&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1oG2_HBffl7G8a-GKZ5xRdhxYDlA&w=1280';
         }
-        
+
         // chainsaw man vol 1
-        if (productID == 36) {
+        if (title == "Chainsaw Man, Vol. 1") {
             image2 = 'https://books.google.com/books/publisher/content?id=54T-DwAAQBAJ&pg=PA5&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U2OQFV2lRrCWJ8beYGUMiwxyP0HqA&w=1280';
             image3 = 'https://books.google.com/books/publisher/content?id=54T-DwAAQBAJ&pg=PA11&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1wjGzmwxNim2NpKZ1kkPKfSkrvMg&w=1280';
         }
